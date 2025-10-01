eea80e967b505f7f27a88775007ae6b52d81c332
mastro94 <francesco.mastro94@gmail.com>
2025-10-01 08:29:56 +0200

    wsgi updated and added to gitignore

--- BE/noteboard_pythonanywhere_com_wsgi.py
+++ BE/noteboard_pythonanywhere_com_wsgi.py
@@ -1,16 +1,19 @@
-import sys
-import os
+import os, sys
+from urllib.parse import quote_plus
 
-# Percorso alla tua app
 project_home = os.path.expanduser('~/noteboard-api')
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Variabili d'ambiente (mettile anche in "Environment variables" nella dashboard se preferisci)
-os.environ.setdefault("API_KEY", "abc123")
-# SQLite (file nella home dell'utente PA)
-os.environ.setdefault("DB_URL", "mysql+pymysql://noteboard:Aa1Bb2Cc3@noteboard.mysql.pythonanywhere-services.com/noteboard$default")
-# consenti CORS: localhost + GitHub Pages (sostituisci user/repo)
+DB_USER = "noteboard"
+DB_PASS = os.environ.get("MYSQL_PASS", "Aa1Bb2Cc3")
+DB_HOST = "noteboard.mysql.pythonanywhere-services.com"
+DB_NAME = "noteboard$notes"
+
+SAFE_PASS = quote_plus(DB_PASS)  # evita problemi con @ : / # ecc.
+os.environ["DB_URL"] = f"mysql+pymysql://{DB_USER}:{SAFE_PASS}@{DB_HOST}/{DB_NAME}"
+
+# (opzionale) origins FE
 os.environ.setdefault("ALLOWED_ORIGINS", "http://localhost:5173,https://<user>.github.io,https://<user>.github.io/<repo>")
 
-from app import app as application  # <-- Flask 'app'
+from app import app as application
