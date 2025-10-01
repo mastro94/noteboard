352f0de1af7bcb7058137520672347812a31e769
mastro94 <francesco.mastro94@gmail.com>
2025-10-01 08:59:24 +0200

    log di bootstrap + catcher globale

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+import './styles.css'
+
+console.log('[Noteboard] client bootstrap')
+
+window.addEventListener('error', (e) => {
+  console.error('[Noteboard] window error:', e?.error || e?.message || e)
+})
+window.addEventListener('unhandledrejection', (e) => {
+  console.error('[Noteboard] unhandledrejection:', e?.reason || e)
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
