d8a5b0e4724a7844bee5640e2b6473a70068fe3a
mastro94 <francesco.mastro94@gmail.com>
2025-10-05 12:52:01 +0200

    fix local vs remote

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// In dev serviamo a base '/', in prod (GitHub Pages) serviamo sotto '/noteboard/'
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/noteboard/', // repo name
+  base: isProd ? '/noteboard/' : '/',
 })
